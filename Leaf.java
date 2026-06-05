5adebb37e1048360a80026cd1ee5ab13e42b3864
bobdermeister123456x-ship-it <bobdermeister123456x@gmail.com>
2026-06-05 10:08:33 +0200

    lkfdjh

--- Leaf.java
+++ Leaf.java
@@ -8,10 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Leaf extends Item
 {
-    /**
-     * Act - do whatever the Leaf wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Leaf(){
+        GreenfootImage img = new GreenfootImage("InventorySprites/Blatt.png");
+        img.scale(150, 150);
+        setImage(img);
+    }
     public void act()
     {
         // Add your action code here.
